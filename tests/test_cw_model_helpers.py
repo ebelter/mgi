93f77a4e74ec54eb4b501607bd2e458680662839
Eddie Belter <ebelter@gmail.com>
2022-06-06 16:35:05 -0500

    Pipeline Tests
    use predetermined id and name

--- tests/test_cw_model_helpers.py
+++ tests/test_cw_model_helpers.py
@@ -2,6 +2,9 @@ import os, unittest
 
 from tests.test_cw_base import BaseWithDb
 class ModelHelpersTest(BaseWithDb):
+    def setUp(self):
+        self.pipeline_id = 10001
+        self.pipeline_name = "__TESTER__"
 
     def test_resolve_features(self):
         from cw.model_helpers import resolve_features, pipeline_features
@@ -25,21 +28,22 @@ class ModelHelpersTest(BaseWithDb):
         with self.assertRaisesRegex(Exception, "No value given for feature: name"):
             resolve_features(["name="], known_features)
 
-    def test_pipeline_helpers(self):
+    def test01_pipeline_helpers(self):
         from cw import db, Pipeline
         from cw.model_helpers import get_pipeline
 
-        p = Pipeline(name="align", wdl="wdl", imports=None)
+        p = Pipeline(id=self.pipeline_id, name=self.pipeline_name, wdl="wdl", imports=None)
         db.session.add(p)
         db.session.commit()
-        self.assertEqual(p.id, 1)
-        p2 = get_pipeline(1)
+        self.assertEqual(p.id, self.pipeline_id)
+
+        p2 = get_pipeline(self.pipeline_id)
         self.assertTrue(p2)
         self.assertEqual(p2, p)
-        p2 = get_pipeline("1")
+        p2 = get_pipeline(str(self.pipeline_id))
         self.assertTrue(p2)
         self.assertEqual(p2, p)
-        p3 = get_pipeline("align")
+        p3 = get_pipeline(self.pipeline_name)
         self.assertTrue(p3)
         self.assertEqual(p3, p)
         pnone = get_pipeline("foo")
