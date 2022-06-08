f4584b5bdd0421d367ff230a0c7c74bd543b4b27
Eddie Belter <ebelter@gmail.com>
2022-06-08 12:41:20 -0500

    Workflow Metadata
    command uses metadata_for_wf function

--- cw/wf_metadata.py
+++ cw/wf_metadata.py
@@ -1,27 +1,36 @@
 import click, json, os, sys
-
+from cw.model_helpers import get_wf
 import cw.server
 
 @click.command(short_help="get metadata of a workflow")
-@click.argument("workflow-id", required=True, nargs=1)
-def metadata_cmd(workflow_id):
+@click.argument("identifier", required=True, nargs=1)
+def metadata_cmd(identifier):
     """
     Get Workflow Metadata
+
+    Give workflow cromwell id, name, or local db id.
+
+    Use 'list' command to see workflow, adn 'add' command to add workflows to the local db.
     """
-    server = cw.server.server_factory()
-    url = f"{server.url()}/api/workflows/v1/{workflow_id}/metadata?excludeKey=submittedFiles&expandSubWorkflows=true"
-    response = server.query(url)
-    if not response or not response.ok:
-        sys.stderr.write(f"Failed to get response from server at {url}\n")
-        return 3
-    sys.stdout.write(f"{json.dumps(json.loads(response.content.decode()), indent=4)}")
+    wf = get_wf(identifier)
+    if wf is None:
+        sys.stderr.write(f"Failed to get workflow for <{identifier}>\n")
+        sys.exit(1)
+    try:
+        metadata = metadata_for_wf(wf)
+    except Exception as e:
+        sys.stderr.write(f"Failed to get metdata workflow <{wf.wf_id}>: {e.args[0]}\n")
+        sys.exit(1)
+    sys.stdout.write(f"{json.dumps(metadata, indent=4)}")
 #-- metadata_cmd
 
 def metadata_for_wf(wf):
     server = cw.server.server_factory()
+    if not server.is_running():
+        raise Exception(f"Cromwell server is not running at <{server.url()}>")
     url = f"{server.url()}/api/workflows/v1/{wf.wf_id}/metadata?excludeKey=submittedFiles&expandSubWorkflows=true"
     response = server.query(url)
     if not response or not response.ok:
-        return None
+        raise Exception(f"Server error encountered getting metadata with <{url}>")
     return json.loads(response.content.decode())
 #-- metadata_for_wf
--- tests/test_cw_wf_metadata.py
+++ tests/test_cw_wf_metadata.py
@@ -1,56 +1,91 @@
-import click, os, requests, tempfile, unittest
+import click, json, os, tempfile, unittest
 from click.testing import CliRunner
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, Mock, patch
+import cw.server
 
 from tests.test_cw_base import BaseWithDb
 class CwWfTest(BaseWithDb):
-    def setUp(self):
-        from cw import appcon
-        appcon.set(group="lsf", name="queue", value="general")
-        appcon.set(group="lsf", name="job_group", value="job")
-        appcon.set(group="lsf", name="user_group", value="user")
-        self.server_host = "compute1-exec-225.ris.wustl.edu"
-        self.server_port = "8888"
-        appcon.set(group="server", name="host", value=self.server_host)
-        appcon.set(group="server", name="port", value=self.server_port)
+    def _setUpClass(self):
+        self.metadata_str = b'{\n    "workflowName": "hic",\n    "id": "9d5ffbbc-b246-449b-9685-9db84016c44e"\n}'
+        self.metadata = json.loads(self.metadata_str.decode())
+        self.add_workflow_to_db(self)
 
-    @patch("requests.get")
-    def test_metadata_cmd(self, requests_p):
+    @patch("cw.server.server_factory")
+    def test_metadata_for_wf(self, factory_p):
+        from cw.wf_metadata import metadata_for_wf as fun
+
+        server = Mock()
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": False})
+        factory_p.return_value = server
+        with self.assertRaisesRegex(Exception, "Cromwell server is not running at <__URL__>"):
+            fun(self.wf)
+        factory_p.assert_called()
+        factory_p.reset_mock()
+
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": True, "query.return_value": None})
+        with self.assertRaisesRegex(Exception, "Server error encountered getting metadata with <__URL__"):
+            fun(self.wf)
+        factory_p.assert_called_once()
+        factory_p.reset_mock()
+
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": True, "query.return_value": Mock(ok=False)})
+        with self.assertRaisesRegex(Exception, "Server error encountered getting metadata with <__URL"):
+            fun(self.wf)
+        factory_p.assert_called_once()
+        factory_p.reset_mock()
+
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": True, "query.return_value": Mock(ok=True, content=self.metadata_str)})
+        metadata = fun(self.wf)
+        self.assertDictEqual(metadata, self.metadata)
+        factory_p.assert_called_once()
+        server.query.assert_called_once()
+
+    @patch("cw.server.server_factory")
+    def test_metadata_cmd(self, factory_p):
         from cw.wf_metadata import metadata_cmd as cmd
-        runner = CliRunner()
 
+        runner = CliRunner()
         result = runner.invoke(cmd, ["--help"])
         self.assertEqual(result.exit_code, 0)
 
-        content = b'{\n    "workflowName": "hic",\n    "id": "9d5ffbbc-b246-449b-9685-9db84016c44e"\n}'
-        response = MagicMock(ok=False, content=content)
-        requests_p.return_value = response
-
-        os.chdir(self.temp_d.name)
-        result = runner.invoke(cmd, ["WF_ID"], catch_exceptions=False)
+        result = runner.invoke(cmd, ["BLAH"])
         try:
-            self.assertEqual(result.exit_code, 0)
+            self.assertEqual(result.exit_code, 1)
         except:
             print(result.output)
             raise
-        expected_output = f"""Failed to get response from server at http://compute1-exec-225.ris.wustl.edu:8888/api/workflows/v1/WF_ID/metadata?excludeKey=submittedFiles&expandSubWorkflows=true
+        expected_output = """Failed to get workflow for <BLAH>
 """
         self.assertEqual(result.output, expected_output)
-        requests_p.assert_called()
 
-        response = MagicMock(ok=True, content=content)
-        requests_p.return_value = response
+        server = Mock()
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": False})
+        factory_p.return_value = server
+        result = runner.invoke(cmd, [f"{self.wf.wf_id}"])
+        try:
+            self.assertEqual(result.exit_code, 1)
+        except:
+            print(result.output)
+            raise
+        expected_output = f"Failed to get metdata workflow <{self.wf.wf_id}>: Cromwell server is not running at <__URL__>\n"
+        self.assertEqual(result.output, expected_output)
+        factory_p.assert_called_once()
+        factory_p.reset_mock()
+        #server.is_running.assert_called_once()
+        server.reset_mock()
 
-        os.chdir(self.temp_d.name)
-        result = runner.invoke(cmd, ["WF_ID"], catch_exceptions=False)
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": True, "query.return_value": Mock(ok=True, content=self.metadata_str)})
+        result = runner.invoke(cmd, [f"{self.wf.wf_id}"])
         try:
             self.assertEqual(result.exit_code, 0)
         except:
             print(result.output)
             raise
-        expected_output = f"{content.decode()}"
+        expected_output = f"{self.metadata_str.decode()}"
         self.assertEqual(result.output, expected_output)
-        requests_p.assert_called()
+        factory_p.assert_called_once()
+        server.is_running.assert_called_once()
+        server.query.assert_called_once()
 #--
 
 if __name__ == '__main__':
