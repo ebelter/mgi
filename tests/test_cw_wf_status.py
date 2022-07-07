3b1c61b74188c5704d96e4486b7d1321ac0936ca
Eddie Belter <ebelter@gmail.com>
2022-07-06 21:22:50 -0500

    WF Status
    get WF from DB
    updste test to use mock server

--- tests/test_cw_wf_status.py
+++ tests/test_cw_wf_status.py
@@ -1,71 +1,50 @@
-import click, os, requests, tempfile, unittest
+import click, os, tempfile, unittest
 from click.testing import CliRunner
-from unittest.mock import MagicMock, patch
+from unittest.mock import Mock, patch
 
 from tests.test_cw_base import BaseWithDb
 class CwWfTest(BaseWithDb):
-    def setUp(self):
-        from cw import appcon
-        appcon.set(group="lsf", name="queue", value="general")
-        appcon.set(group="lsf", name="job_group", value="job")
-        appcon.set(group="lsf", name="user_group", value="user")
-        self.server_host = "compute1-exec-225.ris.wustl.edu"
-        self.server_port = "8888"
-        appcon.set(group="server", name="host", value=self.server_host)
-        appcon.set(group="server", name="port", value=self.server_port)
+    def _setUpClass(self):
+        self.add_workflow_to_db(self)
 
-    def test_wf_cli(self):
-        runner = CliRunner()
-        from cw.wf import cli
-
-        result = runner.invoke(cli, ["--help"])
-        self.assertEqual(result.exit_code, 0)
-        result = runner.invoke(cli, [])
-        self.assertEqual(result.exit_code, 0)
-
-#curl --connect-timeout 5 --max-time 10 -s http://compute1-exec-226.ris.wustl.edu:8888/api/workflows/v1/c808fe24-0edd-46c4-ba23-ff881725e297/status
-#{"status":"Succeeded","id":"c808fe24-0edd-46c4-ba23-ff881725e297"}
-
-    @patch("requests.get")
-    def test_status_cmd(self, requests_p):
+    @patch("cw.server.server_factory")
+    def test_status_cmd(self, server_p):
         from cw.wf_status import status_cmd as cmd
         runner = CliRunner()
 
         result = runner.invoke(cmd, ["--help"])
         self.assertEqual(result.exit_code, 0)
 
-        response = MagicMock(ok=False, content=b'{"status":"Succeeded","id":"WF_ID"}')
-        requests_p.return_value = response
+        server = Mock()
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": False, "query.return_value": Mock(ok=False)})
+        server_p.return_value = server
 
         os.chdir(self.temp_d.name)
-        result = runner.invoke(cmd, ["WF_ID"], catch_exceptions=False)
+        result = runner.invoke(cmd, [str(self.wf.id)], catch_exceptions=False)
         try:
             self.assertEqual(result.exit_code, 0)
         except:
             print(result.output)
             raise
-        expected_output = f"""URL: http://compute1-exec-225.ris.wustl.edu:8888/api/workflows/v1/WF_ID/status
-Failed to get response from server at http://compute1-exec-225.ris.wustl.edu:8888/api/workflows/v1/WF_ID/status
+        expected_output = f"""Failed to get response from server at __URL__/api/workflows/v1/{self.wf.wf_id}/status
 """
         self.assertEqual(result.output, expected_output)
-        requests_p.assert_called()
+        server_p.assert_called()
 
-        response = MagicMock(ok=True, content=b'{"status":"Succeeded","id":"WF_ID"}')
-        requests_p.return_value = response
+        server.configure_mock(**{"url.return_value": "__URL__", "is_running.return_value": True, "query.return_value": Mock(ok=True, content='{"status":"Succeeded","id":"__WF_ID__"}'.encode())})
 
         os.chdir(self.temp_d.name)
-        result = runner.invoke(cmd, ["WF_ID"], catch_exceptions=False)
+        result = runner.invoke(cmd, [str(self.wf.id)], catch_exceptions=False)
         try:
             self.assertEqual(result.exit_code, 0)
         except:
             print(result.output)
             raise
-        expected_output = f"""URL: http://compute1-exec-225.ris.wustl.edu:8888/api/workflows/v1/WF_ID/status
-Workflow ID: WF_ID
+        expected_output = f"""Workflow ID: {self.wf.wf_id}
 Status:      Succeeded
 """
         self.assertEqual(result.output, expected_output)
-        requests_p.assert_called()
+        server_p.assert_called()
 #--
 
 if __name__ == '__main__':
