35e0b23d617a3577521c80d735b3d2560ec957d1
Eddie Belter <ebelter@gmail.com>
2023-11-20 17:10:54 -0600

    CIG Metrics Alignment Statsfile Parser

---
+++ cig/metrics/alignment/parser.py
@@ -0,0 +1,25 @@
+from cig.metrics.alignment.samtools import parse as samtools_parser
+from cig.metrics.alignment.vg import parse as vg_parser
+
+class StatsfileParser(object):
+    def __init__(self, statsfile):
+        self.statsfile = statsfile
+        self.kind, self.parser = get_parser(statsfile)
+
+    def parse(self):
+        with open(self.statsfile, "r") as f:
+            return self.parser(f)
+#-- StatsfileParser
+
+def get_parser(statsfile):
+    with open(statsfile, "r") as f:
+        firstl = f.readline()
+        if firstl.startswith("# This file was produced by samtools stats"):
+            metrics_kind = "samtools_stat"
+            return "samtools", samtools_parser
+        elif firstl.startswith("Total alignments: "):
+            metrics_kind = "vg_stat"
+            return "vg", vg_parser
+        else:
+            raise Exception(f"Failed to determine stats file kind for {statsfile}")
+#-- get_parser
---
+++ tests/cig/test_metrics_alignment_parser.py
@@ -0,0 +1,44 @@
+import click, os, tempfile, unittest
+from io import StringIO
+
+class MetricsAlignmentParserTest(unittest.TestCase):
+    def setUp(self):
+        self.data_dn = os.path.join(os.path.dirname(__file__), "data", "metrics")
+        self.samtools_statsfile = os.path.join(self.data_dn, "samtools.stat")
+        self.vg_statsfile = os.path.join(self.data_dn, "vgstat.stat")
+        self.unknown_statsfile = os.path.join(self.data_dn, "sample.fasta")
+
+    def test_get_parser(self):
+        from cig.metrics.alignment.parser import get_parser
+        from cig.metrics.alignment.samtools import parse as samtools_parse
+        from cig.metrics.alignment.vg import parse as vg_parse
+
+        kind, parser = get_parser(self.samtools_statsfile)
+        self.assertEqual(kind, "samtools")
+        self.assertEqual(parser, samtools_parse)
+        kind, parser = get_parser(self.vg_statsfile)
+        self.assertEqual(kind, "vg")
+        self.assertEqual(parser, vg_parse)
+
+        with self.assertRaisesRegex(Exception, f"Failed to determine stats file kind for {self.unknown_statsfile}"):
+            get_parser(self.unknown_statsfile)
+
+    def test_init(self):
+        from cig.metrics.alignment.parser import StatsfileParser
+        parser = StatsfileParser(self.samtools_statsfile)
+        self.assertTrue(bool(parser))
+        self.assertEqual(parser.statsfile, self.samtools_statsfile)
+        self.assertEqual(parser.kind, "samtools")
+        self.assertTrue(bool(parser.parser))
+
+    def test_parse(self):
+        from cig.metrics.alignment.parser import StatsfileParser
+        parser = StatsfileParser(self.samtools_statsfile)
+        self.assertTrue(bool(parser))
+        metrics = parser.parse()
+        self.assertTrue(bool(metrics))
+        self.assertEqual(type(metrics), dict)
+#--
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
