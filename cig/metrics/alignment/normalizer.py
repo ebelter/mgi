e529ffad400d924ebd2a3b51807d72e9031637b2
Eddie Belter <ebelter@gmail.com>
2023-11-20 19:50:02 -0600

    CIG Metrics
    normalizer

---
+++ cig/metrics/alignment/normalizer.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def metric_names_mapper():
+    data = {
+            "total":         ["sequences", "alignments"],
+            "aligned":       ["reads mapped", "aligned"],
+            "secondary":     ["non-primary alignments", "secondary"],
+            "paired":        ["reads paired", "primary"],
+            "unmapped":      ["reads unmapped", "unmapped"], 
+            "quality mean":  ["average quality", "mapping quality mean"]
+            }
+    return pd.DataFrame(index=["samtools", "vg"], data=data)
+#-- metric_names_mapper
+
+def normalize(kind, metrics):
+    mapper = metric_names_mapper()
+    normalized = {}
+    for name in mapper.columns:
+        normalized[name] = metrics.get(mapper.loc[kind][name], 0)
+
+    normalized["aligned pct"] = round(normalized["aligned"] / normalized["total"], 4)
+    normalized["paired pct"] = round(normalized["paired"] / normalized["total"], 4)
+    normalized["secondary pct"] = round(normalized["secondary"] / normalized["total"], 4)
+    normalized["unmapped pct"] = round(normalized["unmapped"] / normalized["total"], 4)
+    return normalized
+#-- normalize
